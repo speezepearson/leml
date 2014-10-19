4c65a16cc324604328e7ecf373c482a25f7d5bc6
Spencer Pearson <speeze.pearson@gmail.com>
2014-10-19 15:38:17 -0700

    allowed guessing format from extension; revamped demos

--- __main__.py
+++ __main__.py
@@ -5,10 +5,16 @@ import markup
 
 parser = argparse.ArgumentParser()
 parser.add_argument('module_file')
-parser.add_argument('-f', '--format', default='txt')
+parser.add_argument('source', nargs="?")
+parser.add_argument('-f', '--format')
 
 args = parser.parse_args()
 
 modules = markup.parse_modules_json(open(args.module_file))
 
-print(markup.compile(modules, args.format, sys.stdin.read()))
+format = (args.format if args.format is not None
+	      else args.source.rsplit('.', 1)[-1] if (args.source is not None and '.' in args.source)
+	      else 'txt')
+source = sys.stdin if args.source is None else open(args.source)
+
+print(markup.compile(modules, format, source.read()))
